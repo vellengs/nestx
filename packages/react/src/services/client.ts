ace20d2de61e4d228b4a6b49d015d9fff8b9ebef
vellengs <vellengs@qq.com>
2019-03-30 10:13:23 +0800

    fix tslint

--- packages/react/src/services/client.ts
+++ packages/react/src/services/client.ts
@@ -2,13 +2,11 @@ import { Configuration, CoreApi, AppApi, DefaultApi } from '../generated';
 const config: Configuration = {
   basePath: 'http://localhost:5600',
 };
-
 export class Client {
   private static client: Client;
   public coreApi = new CoreApi(config);
   public defaultApi = new DefaultApi(config);
   public appApi = new AppApi(config);
-
   public static get instance() {
     if (!this.client) {
       this.client = new Client();
@@ -16,8 +14,6 @@ export class Client {
     }
     return this.client;
   }
-
   private constructor() {}
 }
-
 export const HttpClient = Client.instance;
